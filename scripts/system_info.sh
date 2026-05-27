4408263f89d1f9eace91ae7fab2f80a76b0ed638
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-05-27 13:44:42 -0400

    added sys info script and Rootkit detection

---
+++ scripts/system_info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "======================================"
+echo " System Information"
+echo "======================================"
+
+echo "[+] Hostname:"
+hostname
+
+echo ""
+echo "[+] Kernel Version:"
+uname -r
+
+echo ""
+echo "[+] Logged-in Users:"
+who
+
+echo ""
+echo "[+] Uptime:"
+uptime
